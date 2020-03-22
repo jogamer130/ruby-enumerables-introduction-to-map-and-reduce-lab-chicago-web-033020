5dfd292c921362909424e849283e332c9cb25e6f
Derek Williams <jogamer130@gmail.com>
2020-03-22 18:01:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,18 +3,18 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
-  source_array.map {|x| x}
+  source_array.map { |x| x }
 end
 
 def map_to_double(source_array)
-  source_array.map {|x| x*2}
+  source_array.map { |x| x*2 }
 end
 
 def map_to_square(source_array)
-  source_array {|x| x**2}
+  source_array.map { |x| x**2 }
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   return source_array.reduce(starting_point){|total, element| total+element}
 end
 
